939e2e66552f928cbdc5f38523e4e3a364747e19
Sai <Sai@tez>
2018-12-27 03:45:33 -0800

    tezz:Committing RestAssured to GitHub

---
+++ RestAssuredProject/src/main/java/webServices/Testcases/TC1_Get_Request.java
@@ -0,0 +1,14 @@
+package webServices.Testcases;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+
+public class TC1_Get_Request {
+
+	@Test
+	public void getWeatherDetails()
+	{
+		RestAssured.baseURI
+	}
+}
